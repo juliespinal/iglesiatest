5075b2f2e6b1ef3f9393ef2b222293ca3efb9b51
Julio Espinal <73973029+juliespinal@users.noreply.github.com>
2022-03-23 16:32:11 -0300

    Update jquery.countdown.js

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -31,7 +31,7 @@
 		(function tick(){
 			
 			// Time left
-			left = Math.floor((options.timestamp - (new Date())) / 1000);
+			left = Math.floor((options.timestamp - (new Date())) / 500);
 			
 			if(left < 0){
 				left = 0;
